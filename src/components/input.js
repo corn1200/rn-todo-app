7230a97a5ca06ec2ff45c80e8128a9412c2df819
corn1200 <corn120033@gmail.com>
2021-12-24 10:50:06 +0900

    화면이 회전해도 화면의 정확한 크기를 재도록 한다

--- src/components/input.js
+++ src/components/input.js
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { Dimensions } from "react-native";
+import { Dimensions, useWindowDimensions } from "react-native";
 
 // 할일 입력을 위한 인풋 컴포넌트(StyledInput) 생성(styled.TextInput)
 // 인풋 컴포넌트는 화면의 크기(${({width})) 보다 40px 작은 넓이(=> width - 40}px)를 가진다
@@ -15,9 +15,9 @@ const StyledInput = styled.TextInput`
 `;
 
 // 할일 입력 컴포넌트(StyledInput)를 반환(return)하는 함수 컴포넌트 생성(Input)
-// 현재 기기의 넓이 값(Dimensions.get('window').width)을 props로 넘겨준다 
+// 현재 기기의 넓이 값(useWindowDimensions().width)을 props로 넘겨준다 
 const Input = () => {
-    const width = Dimensions.get('window').width;
+    const width = useWindowDimensions().width;
     return <StyledInput width={width}/>
 };
 
