55e4ebc5b640c54ec3a483382753d34560a5f34f
corn1200 <corn120033@gmail.com>
2021-12-24 11:27:35 +0900

    props로 속성 설정 및 props 값 타입과 필수 여부 설정

--- src/components/input.js
+++ src/components/input.js
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import { Dimensions, useWindowDimensions } from "react-native";
+import PropTypes from 'prop-types';
 
 // 할일 입력을 위한 인풋 컴포넌트(StyledInput) 생성(styled.TextInput)
 // 인풋 컴포넌트는 화면의 크기(${({width})) 보다 40px 작은 넓이(=> width - 40}px)를 가진다
@@ -17,11 +18,32 @@ const StyledInput = styled.TextInput.attrs(({ theme }) => ({
 `;
 
 // 할일 입력 컴포넌트(StyledInput)를 반환(return)하는 함수 컴포넌트 생성(Input)
-// 현재 기기의 넓이 값(useWindowDimensions().width)을 props로 넘겨준다 
-const Input = ({ placeholder }) => {
+// 현재 기기의 넓이 값(useWindowDimensions().width)을 props로 넘겨준다
+// 입력 최대 길이(maxLength)와 자동 대문자 입력(autoCapitalize) 방지
+// 자동 문법(autoCorrect) 방지, 타이핑 키 타입(returnKeyType)을 설정
+const Input = ({ placeholder, value, onChangeText }) => {
     const width = useWindowDimensions().width;
-    return <StyledInput width={width} placeholder={placeholder} />
+    return <StyledInput 
+                width={width} 
+                placeholder={placeholder} 
+                maxLength={50}
+                autoCapitalize="none"
+                autoCorrect={false}
+                returnKeyType="done"
+                keyboardAppearance="dark"
+                // 인풋 컴포넌트가 렌더링 될 때 props 값으로 
+                // 인풋 안의 값과 변경 이벤트 설정
+                value={value}
+                onChangeText={onChangeText}
+            />;
 };
+
+// props로 넘어와야 하는 타입과 필수 값 여부 설정
+Input.PropTypes = {
+    placeholder: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    onChangeText: PropTypes.func.isRequired
+}
 
 // 스타일 정의 컴포넌트를 모듈로 사용할 수 있도록 설정
 export default Input;
